2b961f1828e8f92c13b05de8f11ef65e57f29824
Ricardo Vargas <rkrdo.vrgs@gmail.com>
2019-09-26 01:38:49 -0400

    updated to latest

---
+++ src/moq.ts
@@ -0,0 +1,118 @@
+﻿import { PromiseMockQueue } from "./promise-mock-queue";
+
+/**
+ * Defines a class to create mock objects
+ */
+export class Mock<T> {
+    object: T;
+
+    static promiseQueue: PromiseMockQueue = new PromiseMockQueue();
+
+    /**
+     * Used to create an instance of a mock of T
+     * @param {T} Constructor?
+     */
+    constructor(Constructor?: T) {
+        this.object = !!Constructor ? new (<any>Constructor)() : <T>{};
+    }
+
+    /**
+     * Gets the name of the property based on a selector
+     * @param {Expression<Func<T>>} propSelector - property selector to get the property name
+     * @returns the property name
+     */
+    private getPropertyName(propSelector: (obj: T) => any): string {
+        let propName: string,
+            pattern = new RegExp("(=>|return)\\s([a-zA-Z_$][a-zA-Z0-9_$]*\\.?)+"),
+            matches = pattern.exec(propSelector.toString());
+
+        if (!matches || !matches[0]) { throw "propSelector regex exception"; }
+
+        propName = matches[0].replace("return ", "").trim();
+        if (propName.indexOf(".") !== -1) {
+            propName = propName.substring(propName.indexOf(".") + 1);
+        }
+
+        return propName;
+    }
+
+    /**
+     * Sets the value of a property by name
+     * @param {string} propName - name of the property
+     * @param {C} value - value to be assigned to the property
+     * @returns the value that was assigned
+     */
+    private defineDeepProperty<C>(propName: string, value: C): C {
+        let segments = propName.split("."),
+            segmentLen = segments.length,
+            nextObj = this.object,
+            prevObj;
+
+        for (let i = 0; i < segmentLen; i++) {
+            let propNameSegment = segments[i];
+            prevObj = nextObj;
+            nextObj = nextObj[propNameSegment];
+            if (nextObj === undefined || nextObj === null) {
+                // not the last property segment
+                // means complex object should be created
+                // else it means it is the actual fuction to mock
+                if (segmentLen - i > 1) {
+                    prevObj[propNameSegment] = {};
+                } else {
+                    prevObj[propNameSegment] = value;
+                }
+            }
+        }
+
+        return value;
+    }
+
+    /**
+     * Creates the custom spy structure
+     * @param {string} name - name of the spy
+     * @returns a custom spy with promise functionality
+     */
+    private createSpy(name: string): ISpy {
+        let spy: ISpy = <ISpy>jasmine.createSpy(name);
+
+        spy.and.callFake.promise = {
+            resolve: (fn: Function): ISpy => {
+                return <ISpy>spy.and.callFake(function () {
+                    let value = fn.apply(fn, arguments);
+                    return Mock.promiseQueue.resolve(value);
+                });
+            },
+            reject: (fn: Function): ISpy => {
+                return <ISpy>spy.and.callFake(function () {
+                    let value = fn.apply(fn, arguments);
+                    return Mock.promiseQueue.reject(value);
+                });
+            }
+        };
+
+        return spy;
+    }
+
+    /**
+     * Creates a spy on a given property
+     * @param {Expression<Func<T>>} propSelector - property selector to get the property name
+     * @returns a custom spy with promise capabilities
+     */
+    spyOn(propSelector: (obj: T) => any): ISpy {
+        let propName = this.getPropertyName(propSelector),
+            spy: ISpy = this.createSpy(propName);
+
+
+        return this.defineDeepProperty(propName, spy);
+    }
+
+    /**
+     * Sets a property value on a given property
+     * @param {Expression<Func<T>>} propSelector - property selector to get the property name
+     * @param {C} value - value to be assigned to the property
+     */
+    define<C>(propSelector: (obj: T) => C, value: C) {
+        let propName = this.getPropertyName(propSelector);
+        this.defineDeepProperty(propName, value);
+    }
+}
---
+++ src/promise-mock-queue.ts
@@ -0,0 +1,102 @@
+export interface IMockQueueWaitConfig {
+    ms?: number;
+    jasmineClockEnabled?: boolean;
+}
+
+/**
+ * Defines a class to create promise queue
+ */
+export class PromiseMockQueue {
+    asyncQueue: Promise<any>[];
+
+    /**
+     * Creates an instance of a promise queue
+     */
+    constructor() {
+        this.asyncQueue = [];
+    }
+
+    /**
+     * Creates a promise that resolves itself
+     * @param {T} value - value to be returned once the promise has completed
+     * @returns a promise mock
+     */
+    resolve<T>(value: T): Promise<T> {
+        let promise = Promise.resolve(value);
+        this.asyncQueue.push(promise);
+        return promise;
+    }
+
+    /**
+     * Creates a promise that rejects itself
+     * @param {T} value - value error to be returned once the promise has erroed
+     * @returns a promise mock
+     */
+    reject<T>(value: T): Promise<void> {
+        let promise = Promise.reject(value);
+        this.asyncQueue.push(promise);
+        return promise;
+    }
+
+    /**
+     * Processes all pending promises in queue and empties the queue
+     * @returns a promise to be resolved once all pending promises have been executed
+     */
+    flush(): Promise<any> {
+        let promise = Promise.all(this.asyncQueue);
+        this.reset();
+        return promise;
+    }
+
+    /**
+     * Processes all pending promises in queue, empties the queue, and waits
+     * @returns a promise to be resolved once all pending promises have been executed
+     */
+    flushAndWait(jasmineClockEnabled?: boolean, ms?: number);
+    flushAndWait(config: IMockQueueWaitConfig);
+    flushAndWait(): Promise<any> {
+        const args = arguments;
+        return this.flush().then(() => this.wait.apply(this, args));
+    }
+
+    /**
+     * Resets the promise queue
+     */
+    reset() {
+        this.asyncQueue = [];
+    }
+
+    /**
+     * Adds a delay to process exceptions and/or inner promises inside promises
+     * @param {boolean = false} jasmineClockEnabled
+     * @param {number = 0} ms
+     * @returns
+     */
+    wait(jasmineClockEnabled?: boolean, ms?: number);
+    wait(config: IMockQueueWaitConfig);
+    wait() {
+        let config: IMockQueueWaitConfig = arguments[0],
+            jasmineClockEnabled = arguments[0],
+            ms = arguments[1];
+
+        if (!!config && config instanceof Object && config.constructor === Object) {
+            jasmineClockEnabled = config.jasmineClockEnabled;
+            ms = config.ms;
+        }
+
+        jasmineClockEnabled = jasmineClockEnabled || false;
+        ms = ms || 0;
+
+        if (jasmineClockEnabled) {
+            jasmine.clock().uninstall();
+        }
+        return new Promise(res => {
+            setTimeout(() => {
+                if (jasmineClockEnabled) {
+                    jasmine.clock().install();
+                }
+                res();
+            }, ms);
+        });
+    }
+}
